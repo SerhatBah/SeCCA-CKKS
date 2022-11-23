0b60fc3aeae8415f70f088bf49562b261a71f6a9
serhat <snoop-killa@hotmail.de>
2022-11-23 14:34:08 +0100

    Big Changes in type 2

--- secured_cheng_church_yeast.py
+++ secured_cheng_church_yeast.py
@@ -1,29 +1,26 @@
 import time
 #from biclustlib.algorithms import SecuredChengChurchAlgorithm
-#from biclustlib.datasets import load_yeast_tavazoie
-import numpy as np
-
 from biclustlib.algorithms import SecuredChengChurchAlgorithmType2
+from biclustlib.datasets import load_yeast_tavazoie
+import numpy as np
 
 m0 = time.perf_counter()
 
 # load yeast data used in the original Cheng and Church's paper
-    #data = np.random.randint(0, 5, size=(10, 5))
+data = load_yeast_tavazoie().values
 
 # DATA GENERATION
-num_rows, num_cols = 10, 5
+num_rows, num_cols = data.shape
 n_elements = num_rows*num_cols
-np.random.seed(42)                                          # Fixed seed for reproducibility
-data = np.random.randint(0, 5, size=(num_rows, num_cols))   # Generate data
-
-#load_yeast_tavazoie().values
+np.random.seed(42)          # Fixed seed for reproducibility
+#data = np.random.randint(0, 5, size=(num_rows, num_cols))   # Generate data
 
 # missing value imputation suggested by Cheng and Church
-#missing = np.where(data < 0.0)
-#data[missing] = np.random.randint(low=0, high=800, size=len(missing[0]))
+missing = np.where(data < 0.0)
+data[missing] = np.random.randint(low=0, high=800, size=len(missing[0]))
 
 # creating an instance of the SecuredChengChurchAlgorithm class and running with the parameters
-secca = SecuredChengChurchAlgorithmType2(num_biclusters=5, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
+secca = SecuredChengChurchAlgorithmType2(num_biclusters=num_rows, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
 biclustering = secca.run(data)
 print(biclustering)
 
